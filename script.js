a98838df26b8427080ec854092a4c0a89fe70daa
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2023-02-20 19:39:32 +0200

    landing mobile

--- script.js
+++ script.js
@@ -60,6 +60,94 @@
 }());
 
 
+// (function () {
+//   const sliderWrapper = document.querySelector(".slider__wrapper");
+//   const cards = document.querySelectorAll(".card");
+//   const cardWidth = cards[0].offsetWidth + 48; // Add some margin to the card width
+//   const sliderControls = document.querySelectorAll(
+//     ".slider__control-bar button"
+//   );
+//   let position = 0;
+
+//   // Function to move the slider to the left
+//   function moveSliderLeft() {
+//     position -= cardWidth;
+//     if (position < 0) {
+//       position = 0;
+//     }
+//     sliderWrapper.style.transform = `translateX(-${position}px)`;
+//   }
+
+//   // Function to move the slider to the right
+//   function moveSliderRight() {
+    
+//     position += cardWidth;
+//     if (position > (cards.length - 4) * cardWidth) {
+//       position = (cards.length - 4) * cardWidth;
+//     }
+//     if (window.innerWidth < 1200 && position > (cards.length - 3) * cardWidth) {
+//       console.log((cards.length - 3) * cardWidth);
+//       console.log(position);
+//       position = (cards.length - 3) * cardWidth;
+//     }
+//     sliderWrapper.style.transform = `translateX(-${position}px)`;
+//   }
+
+//   // Add click event listeners to the slider controls
+//   sliderControls[0].addEventListener("click", moveSliderLeft);
+//   sliderControls[1].addEventListener("click", moveSliderRight);
+
+// })
+
+(function () {
+  const sliderItems = document.querySelector(".slider__wrapper");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+
+  let currentPosition = 0;
+
+  function slide(direction) {
+    const itemWidth = sliderItems.querySelector(".card").offsetWidth;
+
+    let maxPosition;
+    
+    maxPosition = (sliderItems.children.length - 4) * itemWidth;
+
+    if (window.innerWidth < 1200)
+      maxPosition = (sliderItems.children.length - 3) * itemWidth;
+
+    if (window.innerWidth < 850)
+      maxPosition = (sliderItems.children.length - 2) * itemWidth;
+
+    if(window.innerWidth < 620)
+      maxPosition = (sliderItems.children.length - 1) * itemWidth;
+      
+    currentPosition += direction * itemWidth;
+    console.log(
+      window.innerWidth,
+      maxPosition,
+      sliderItems.children.length - 3,
+      itemWidth
+    );
+    if (currentPosition > 0) {
+      currentPosition = -maxPosition;
+    } else if (currentPosition < -maxPosition) {
+      currentPosition = 0;
+    }
+    sliderItems.style.transform = `translateX(${currentPosition}px)`;
+  }
+
+  prevButton.addEventListener("click", () => {
+    slide(1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    slide(-1);
+  });
+
+})
+
+
 (function () {
     const categoryLinks = document.querySelectorAll('.category__link');
     let currentCategory;
